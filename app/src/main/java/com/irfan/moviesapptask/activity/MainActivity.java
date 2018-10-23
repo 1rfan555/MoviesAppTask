782ccee554b10d874348c3de62251dc3b07785c1
1rfan555 <irfan.inamdar1234@gmail.com>
2018-10-23 13:54:12 +0530

    hard-coding

--- app/src/main/java/com/irfan/moviesapptask/activity/MainActivity.java
+++ app/src/main/java/com/irfan/moviesapptask/activity/MainActivity.java
@@ -2,9 +2,13 @@ package com.irfan.moviesapptask.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import com.irfan.moviesapptask.R;
+import com.irfan.moviesapptask.adapter.RecyclerTouchListener;
 import com.irfan.moviesapptask.client.ApiClient;
 import com.irfan.moviesapptask.client.ApiInterface;
 import com.irfan.moviesapptask.model.Movie;
@@ -34,13 +38,33 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        //RecyclerView
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        //i'm not doing item touch event by below method...
+        /*recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));*/
 
 
+         //GetClient
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
         Call<MoviesResponse> call = apiService.getUpcomingMovies(API_KEY);
         call.enqueue(new Callback<MoviesResponse>() {
             @Override
             public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+
+
+                //need to create adapter to fetch movie details...
 
             }
 
