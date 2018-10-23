76e9c8bf9620624fa47788580928958e487c2f34
1rfan555 <irfan.inamdar1234@gmail.com>
2018-10-23 13:43:40 +0530

    Client-side

---
+++ app/src/main/java/com/irfan/moviesapptask/client/ApiInterface.java
@@ -0,0 +1,18 @@
+package com.irfan.moviesapptask.client;
+
+import com.irfan.moviesapptask.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/upcoming")
+    Call<MoviesResponse> getUpcomingMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
+
