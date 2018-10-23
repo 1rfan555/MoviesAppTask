dd2db0c23502b013bc1b0f892d1e5a5c7fc3853c
1rfan555 <irfan.inamdar1234@gmail.com>
2018-10-23 14:20:59 +0530

    onCreateOptionsMenu

--- app/src/main/java/com/irfan/moviesapptask/activity/DetailsActivity.java
+++ app/src/main/java/com/irfan/moviesapptask/activity/DetailsActivity.java
@@ -3,6 +3,8 @@ package com.irfan.moviesapptask.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -44,6 +46,24 @@ public class DetailsActivity extends AppCompatActivity {
         tvVoteAvg.setText(String.valueOf(intent.getDoubleExtra("voteAverage",0)));
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.info:
+                startActivity(new Intent(this,MyActivity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
